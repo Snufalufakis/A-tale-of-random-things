2f139b8a2068fbfd549cd762b1acabdbc28d64d9
Snufalufakis <snufalufakis@gmail.com>
2023-01-31 12:13:18 -0800

    bump

--- udemy-Typescript/fetchjson/index.ts
+++ udemy-Typescript/fetchjson/index.ts
@@ -6,17 +6,22 @@ interface Todo {
   id: number;
   title: string;
   completed: boolean;
-}
+};
 
 axios.get(url).then(response => {
   // response data properties
   const todo = response.data as Todo;
-  const ID = todo.id;
+  const id = todo.id;
   const title = todo.title;
   const completed = todo.completed;
+  
+  logTodo(id, title, completed);
+});
 
-  console.log(`
-  The Todo with ID ${ID} has been successfully
-  The ${title} has been successfully
-  An don't forget to update the ${completed} status}`)
-})
+const logTodo = (id: number, title: string, completed: boolean ) => { 
+  // assigns the todo log (tree) to be displayed in a certain order.
+console.log(`
+The Todo with ID ${id} has been successfully
+The ${title} has been successfully
+An don't forget to update the ${completed} status}`);
+};
